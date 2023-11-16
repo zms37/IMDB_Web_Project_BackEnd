937bcb525a74099cdb42ea56e4377e3f1590526e
Zakaria Al Shahary <zma47@mail.aub.edu>
2023-11-16 21:14:20 +0200

    New

--- routes/comingSoon.js
+++ routes/comingSoon.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require('../models/Movie'); // Import your Movie model
 
 // Coming Soon
-router.get('/comingSoon', (req, res) => {
-    // Fetch and return the movies that are coming soon
+router.get('/comingSoon', async (req, res) => {
+    try {
+        const currentDate = new Date();
+        const movies = await Movie.find({ releaseDate: { $gt: currentDate } });
+        res.json(movies);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 module.exports = router;
--- routes/featuredToday.js
+++ routes/featuredToday.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require('../models/Movie'); // Import your Movie model
 
 // Featured Today
-router.get('/featuredToday', (req, res) => {
-    // Fetch and return the featured movies for today
+router.get('/featuredToday', async (req, res) => {
+    try {
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0); // Set the time to 00:00:00
+        const tomorrowDate = new Date(currentDate);
+        tomorrowDate.setDate(tomorrowDate.getDate() + 1); // Set the date to tomorrow
+
+        const movies = await Movie.find({
+            featuredDate: {
+                $gte: currentDate,
+                $lt: tomorrowDate
+            }
+        });
+
+        res.json(movies);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
 module.exports = router;
