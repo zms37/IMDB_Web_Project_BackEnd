f8ffbcb26d9ef62f0ec2522a3fcf2361f37c815a
Zakaria Al Shahary <zma47@mail.aub.edu>
2023-12-03 17:34:41 +0200

    Update userTopPicks.js

--- routes/userTopPicks.js
+++ routes/userTopPicks.js
@@ -23,41 +23,48 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
-// Add or update top picks for a user
+// Add a movie to top picks for a user
 router.post('/:userId', async (req, res) => {
-    if (!isValidObjectId(req.params.userId)) {
-        return res.status(400).json({ message: 'Invalid user ID' });
+    if (!isValidObjectId(req.params.userId) || !isValidObjectId(req.body.movieId)) {
+        return res.status(400).json({ message: 'Invalid user ID or movie ID' });
     }
 
     try {
         let topPicks = await UserTopPicks.findOne({ user: req.params.userId });
         if (!topPicks) {
-            topPicks = new UserTopPicks({ user: req.params.userId, movies: req.body.movies });
+            topPicks = new UserTopPicks({ user: req.params.userId, movies: [req.body.movieId] });
         } else {
-            topPicks.movies = req.body.movies;
+            // Add movie to the list if it's not already there
+            if (!topPicks.movies.includes(req.body.movieId)) {
+                topPicks.movies.push(req.body.movieId);
+            }
         }
         const updatedTopPicks = await topPicks.save();
         res.status(201).json(updatedTopPicks);
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }
 });
 
-// Delete top picks for a user
-router.delete('/:userId', async (req, res) => {
-    if (!isValidObjectId(req.params.userId)) {
-        return res.status(400).json({ message: 'Invalid user ID' });
+// Remove a movie from top picks for a user
+router.delete('/:userId/:movieId', async (req, res) => {
+    if (!isValidObjectId(req.params.userId) || !isValidObjectId(req.params.movieId)) {
+        return res.status(400).json({ message: 'Invalid user ID or movie ID' });
     }
 
     try {
-        const result = await UserTopPicks.findOneAndDelete({ user: req.params.userId });
-        if (!result) {
+        const topPicks = await UserTopPicks.findOne({ user: req.params.userId });
+        if (!topPicks) {
             return res.status(404).json({ message: 'User top picks not found' });
         }
-        res.json({ message: 'User top picks deleted' });
+
+        topPicks.movies = topPicks.movies.filter(movieId => movieId.toString() !== req.params.movieId);
+        await topPicks.save();
+        res.json({ message: 'Movie removed from top picks' });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
+
 
 module.exports = router;
